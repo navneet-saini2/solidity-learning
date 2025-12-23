47a93b0a526865d92450ff4ff1a1641947db393d
navneet <navneet201204@gmail.com>
2025-12-23 15:48:19 +0530

    Gas optimization

---
+++ Level-03/src/GasSqueezer.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+contract GasSqueezer {
+    // --- UNOPTIMIZED (Uses 4 slots/shelves) ---
+    // uint256 a = 1;
+    // uint256 b = 15;
+    // ...
+
+    // --- OPTIMIZED (Uses 1 slot/shelf) ---
+    // Because 64 + 64 + 64 + 64 = 256 bits (Exactly 1 slot)
+    uint64 public firstNo = 1;
+    uint64 public secondNo = 15;
+    uint64 public thirdNo = 20;
+    uint64 public fourthNo = 200;
+
+    // --- IMMUTABLE ---
+    // Why is this cheap? Because it's not stored on a "shelf"!
+    // It is hardcoded directly into the contract's bytecode.
+    address public immutable VAULT_ADDRESS;
+
+    constructor(address _vault) {
+        VAULT_ADDRESS = _vault;
+    }
+}

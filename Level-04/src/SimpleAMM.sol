1c50f7976381514fe6a89920bd943b3fd33e0638
navneet <navneet201204@gmail.com>
2025-12-29 20:20:35 +0530

    Add LP share accounting (totalSupply and balanceOf)

--- Level-04/src/SimpleAMM.sol
+++ Level-04/src/SimpleAMM.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.30;
+pragma solidity ^0.8.26;
 
 interface IERC20 {
     // functions later
@@ -12,6 +12,9 @@ contract SimpleAMM {
 
     uint256 public reserveA;
     uint256 public reserveB;
+
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
 
     // constructor
     constructor(address _tokenA, address _tokenB) {

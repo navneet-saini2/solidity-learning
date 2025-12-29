a93acde34d7e3e988bc5551461058e85622544fd
navneet <navneet201204@gmail.com>
2025-12-29 20:11:53 +0530

    Add SimpleAMM skeleton with tokens, reserves, constructor

--- Level-04/src/SimpleAMM.sol
+++ Level-04/src/SimpleAMM.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity 0.8.30;
+pragma solidity ^0.8.30;
 
 interface IERC20 {
     // functions later
@@ -7,7 +7,17 @@ interface IERC20 {
 
 contract SimpleAMM {
     // state variables
+    IERC20 public immutable tokenA;
+    IERC20 public immutable tokenB;
+
+    uint256 public reserveA;
+    uint256 public reserveB;
+
     // constructor
+    constructor(address _tokenA, address _tokenB) {
+        tokenA = IERC20(_tokenA);
+        tokenB = IERC20(_tokenB);
+    }
     // addLiquidity
     // swap
     // removeLiquidity

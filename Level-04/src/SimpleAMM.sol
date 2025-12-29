a9ccf4e842e283e3f75e18bff098050ababa8a0c
navneet <navneet201204@gmail.com>
2025-12-29 19:52:20 +0530

    change contract name

---
+++ Level-04/src/SimpleAMM.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+interface IERC20 {
+    // functions later
+}
+
+contract SimpleAMM {
+    // state variables
+    // constructor
+    // addLiquidity
+    // swap
+    // removeLiquidity
+}

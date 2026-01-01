8725e0395080197a4f5d3e5dd276b075ac6acf4d
navneet <navneet201204@gmail.com>
2026-01-01 22:50:24 +0530

    add interface ERC20

--- Level-04/src/SimpleAMM.sol
+++ Level-04/src/SimpleAMM.sol
@@ -2,7 +2,17 @@
 pragma solidity ^0.8.26;
 
 interface IERC20 {
-// functions later
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 }
 
 contract SimpleAMM {
